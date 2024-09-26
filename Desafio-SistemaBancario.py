3ec5247138f0ca97d4cdeae6e1510a27912a6414
Bruno Cesar <bcesar2002@hotmail.com>
2024-09-26 16:51:33 -0300

    add sistemabancario.py

---
+++ Desafio-SistemaBancario.py
@@ -0,0 +1,66 @@
+selecao = """
+
+(d/D) Depositar
+(s/S) Sacar
+(e/E) Extrato
+(x/X) Sair
+
+=> """
+
+SAQUES_MAX = 3
+MAIOR_LIMITE = 500
+saldo = 0
+qtd_saques = 0
+saida = ""
+
+while True:
+
+    escolha = input(selecao)
+
+    if escolha == "D" or escolha == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            saida += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif escolha == "S" or escolha == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        saldo_maior = valor > saldo
+
+        passou_limite = valor > MAIOR_LIMITE
+
+        excesso_de_saques = qtd_saques >= SAQUES_MAX
+
+        if saldo_maior:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif passou_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excesso_de_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            saida += f"Saque: R$ {valor:.2f}\n"
+            qtd_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif escolha == "E" or escolha == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not saida else saida)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif escolha == "X" or escolha == "x":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
